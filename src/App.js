3deed17f26625943fc39fa01d1c8be7519e793e5
creator54 <hi.creator54@gmail.com>
2021-05-22 03:37:31 +0530

    add spritesheet to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import img from './sprite.png'
+import { Sprite } from 'react-spritesheet'
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <div className="Dino">
+          <Sprite filename={ img } width={1200} height={115} />
+        </div>
         <header className="App-header">
         </header>
       </div>
