c98c42302192f600d36f563e5d790a5848636dce
creator54 <hi.creator54@gmail.com>
2021-05-22 11:15:51 +0530

    add some sprites

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import img from './sprite.png'
-import { Sprite } from 'react-spritesheet'
+import { SpriteSheet } from 'react-spritesheet'
+
+const spritesheet = {
+  dino: {
+    x: 1338,
+    y: 0,
+    width: 86,
+    height: 100,
+  },
+  cactus1: {
+    x:440,
+    y: 0,
+    width: 40,
+    height: 100,
+  },
+  cactus2: {
+    x: 480,
+    y: 0,
+    width: 35,
+    height: 100,
+  },
+  cactus3: {
+    x: 512,
+    y: 0,
+    width: 35,
+    height: 100,
+  },
+  cactus4: {
+    x: 580,
+    y: 0,
+    width: 35,
+    height: 100,
+  },
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
         <div className="Dino">
-          <Sprite filename={ img } width={1200} height={115} />
+          <SpriteSheet filename={img} data={spritesheet} sprite="dino" />
+          <SpriteSheet filename={img} data={spritesheet} sprite="cactus1" />
+          <SpriteSheet filename={img} data={spritesheet} sprite="cactus2" />
+          <SpriteSheet filename={img} data={spritesheet} sprite="cactus3" />
+          <SpriteSheet filename={img} data={spritesheet} sprite="cactus4" />
         </div>
         <header className="App-header">
         </header>
---
+++ src/Sprites.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Sprites = {
+    dino: {
+      x: 1338,
+      y: 0,
+      width: 86,
+      height: 100,
+    },
+    cactus1: {
+      x:440,
+      y: 0,
+      width: 40,
+      height: 100,
+    },
+    cactus2: {
+      x: 480,
+      y: 0,
+      width: 35,
+      height: 100,
+    },
+    cactus3: {
+      x: 512,
+      y: 0,
+      width: 35,
+      height: 100,
+    },
+    cactus4: {
+      x: 580,
+      y: 0,
+      width: 35,
+      height: 100,
+    },
+  }
+
+  export default Sprites;
