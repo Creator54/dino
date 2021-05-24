3f59eda1bdc77e7df36e037e627ce3fa22a7f4a2
creator54 <hi.creator54@gmail.com>
2021-05-24 18:53:12 +0530

    cleanup + move button to center

--- src/App.js
+++ src/App.js
@@ -2,47 +2,15 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import img from './sprite.png'
 import { SpriteSheet } from 'react-spritesheet'
-import React, { Component } from 'react'
-
-const spritesheet = {
-  dino: {
-    x: 1338,
-    y: 0,
-    width: 86,
-    height: 100,
-  },
-  cactus1: {
-    x:440,
-    y: 0,
-    width: 40,
-    height: 100,
-  },
-  cactus2: {
-    x: 480,
-    y: 0,
-    width: 35,
-    height: 100,
-  },
-  cactus3: {
-    x: 512,
-    y: 0,
-    width: 35,
-    height: 100,
-  },
-  cactus4: {
-    x: 580,
-    y: 0,
-    width: 35,
-    height: 100,
-  },
-}
+import { Component } from 'react'
+import Sprites from './Sprites';
 
 
 export default class App extends Component {
   state={
     class:""
   }
-  handleKeyPress=(event)=>{
+  handleKeyPress=()=>{
       this.setState({class:"jump"});
       // console.log(this.state.class);
       setTimeout(function(){
@@ -53,20 +21,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-    <Router>
-      <div className="game">
-        <div id="dino" className={this.state.class} >
-          <SpriteSheet filename={img} data={spritesheet} sprite="dino" />
-        </div>
-        <div id="cactus" >
-        <SpriteSheet filename={img} data={spritesheet} sprite="cactus1" />
-        </div>
-      </div>
-      <div>
-      <button className="button" onClick={this.handleKeyPress}>Jump</button>
-      </div>
-    </Router>
-  );
+        <Router>
+          <div className="game">
+            <div id="dino" className={this.state.class} >
+              <SpriteSheet filename={img} data={Sprites} sprite="dino" />
+            </div>
+            <div id="cactus" >
+              <SpriteSheet filename={img} data={Sprites} sprite="cactus1" />
+            </div>
+          </div>
+          <div class="wrapper">
+            <button className="button" onClick={this.handleKeyPress}>Jump</button>
+          </div>
+        </Router>
       </div>
     )
   }
--- src/Sprites.js
+++ src/Sprites.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Sprites = {
     dino: {
       x: 1338,
