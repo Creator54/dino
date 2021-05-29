4b320f5b1e46adcb07703e2be39530dbec319d90
creator54 <hi.creator54@gmail.com>
2021-05-29 16:48:35 +0530

    spwan random cactus

--- src/App.js
+++ src/App.js
@@ -5,33 +5,46 @@ import { SpriteSheet } from 'react-spritesheet'
 import { Component } from 'react'
 import Sprites from './Sprites';
 
-
 export default class App extends Component {
-  state={
-    class:""
+    constructor(props) {
+      super(props);
+      this.switchImage = this.switchImage.bind(this);
+      this.state = {
+        random_cactus: 0,
+        cactus_array: [ "cactus1", "cactus2", "cactus3", "cactus4"],
+      }
   }
-  handleKeyPress=()=>{
-      this.setState({class:"jump"});
-      // console.log(this.state.class);
-      setTimeout(function(){
-        this.setState({class:""});
-      }.bind(this),200);
-      
+
+
+  switchImage() {
+    if (this.state.random_cactus < this.state.cactus_array.length - 1) {
+      this.setState({
+        random_cactus: this.state.random_cactus + 1
+      });
+    } else {
+      this.setState({
+        random_cactus: 0
+      });
+    }
+    return this.random_cactus;
   }
+
+  componentDidMount() {
+    setInterval(this.switchImage, 4000);
+  }
+
   render() {
     return (
       <div>
         <Router>
-          <div className="game">
+          <div className="game"   onKeyPress={this.handleKeyPress}>
             <div id="dino" className={this.state.class} >
               <SpriteSheet filename={img} data={Sprites} sprite="dino" />
             </div>
             <div id="cactus" >
-              <SpriteSheet filename={img} data={Sprites} sprite="cactus1" />
+              <SpriteSheet filename={img} data={Sprites} sprite={this.state.cactus_array[this.state.random_cactus]} />
             </div>
-            <button className="button" onClick={this.handleKeyPress}>Jump</button>
           </div>
-          <div class="wrapper">          </div>
         </Router>
       </div>
     )
