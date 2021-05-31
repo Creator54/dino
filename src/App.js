42968a4ca8eab410e0dfe136add214b54232b5e0
jay522 <jayrajverma7777@gmail.com>
2021-05-31 17:27:06 +0530

    dino animation

--- src/App.js
+++ src/App.js
@@ -6,43 +6,44 @@ import { Component } from 'react'
 import Sprites from './Sprites';
 
 export default class App extends Component {
-
-  state={
-    class:""
+  constructor(props) {
+    super(props);
+    this.state={
+      class:"",
+      random_cactus: 1,
+      random_dino:1,
+      cactus_array: [ "cactus1", "cactus2", "cactus3", "cactus4"],
+      dino_array: [ "dino0","dino1","dino0","dino1"],//100
+    }
+    this.switchCactus = this.switchCactus.bind(this);
+    this.switchDino = this.switchDino.bind(this);
   }
 
   handleKeyPress=()=>{
     this.setState({class:"jump"});
-    // console.log(this.state.class);
     setTimeout(function(){
       this.setState({class:""});
     }.bind(this),200);  
   }
 
-  constructor(props) {
-    super(props);
-    this.switchImage = this.switchImage.bind(this);
-    this.state = {
-      random_cactus: 1,
-      cactus_array: [ "cactus1", "cactus2", "cactus3", "cactus4"],
-    }
+  switchCactus() {
+    if (this.state.random_cactus < 3) 
+    {this.setState({ random_cactus: this.state.random_cactus + 1}); }
+     else
+      { this.setState({random_cactus: 0}); }
+    return this.state.random_cactus;
   }
-
-  switchImage() {
-    if (this.state.random_cactus < this.state.cactus_array.length - 1) {
-      this.setState({
-        random_cactus: this.state.random_cactus + 1
-      });
-    } else {
-      this.setState({
-        random_cactus: 0
-      });
-    }
-    return this.random_cactus;
+  switchDino() {
+    if (this.state.random_dino < 3) 
+    { this.setState({ random_dino: this.state.random_dino + 1});} 
+    else 
+    {this.setState({random_dino: 0}); }
+    return this.state.random_dino;
   }
 
   componentDidMount() {
-    setInterval(this.switchImage, 4000);
+    setInterval(this.switchCactus, 3000);
+    setInterval(this.switchDino, 200);
   }
 
   render() {
@@ -51,7 +52,7 @@ export default class App extends Component {
         <Router>
           <div className="game">
             <div id="dino" className={this.state.class} >
-              <SpriteSheet filename={img} data={Sprites} sprite="dino" />
+              <SpriteSheet filename={img} data={Sprites} sprite={this.state.dino_array[this.state.random_dino]} />
             </div>
             <div id="cactus" >
               <SpriteSheet filename={img} data={Sprites} sprite={this.state.cactus_array[this.state.random_cactus]} />
--- src/Sprites.js
+++ src/Sprites.js
@@ -5,6 +5,18 @@ const Sprites = {
       width: 86,
       height: 100,
     },
+    dino0: {
+      x: 1600,
+      y: 0,
+      width: 86,
+      height: 100,
+    },
+    dino1: {
+      x: 1512,
+      y: 0,
+      width: 86,
+      height: 100,
+    },
     cactus1: {
       x:440,
       y: 0,
